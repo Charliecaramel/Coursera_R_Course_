414fcd0310c40d1f61fd2206d2916dc0c9a7ffa7
Charlotte Van Herwijnen <cml.v.herwijnen@gmail.com>
2020-07-15 12:26:34 +0100

    How to construct IF statements in R

---
+++ IF Statments .R
@@ -0,0 +1,28 @@
+#Control Structures
+#1. IF, ELSE statements
+#The general format of IF statements are as follows
+
+# if(condition){
+#     if commands
+#} else{
+#     ele commands
+#}
+
+#else is optional when there are only 2 possible outcomes, typically we also use elseif for many mroe conditions.
+
+# For Example
+#1. with variable specification
+x=4
+if(x>3){
+  y = 0
+}else{
+  y = 1
+}
+y
+#2. without variaable specification
+if(x>3){
+  10
+}else{
+  0
+}
+
