157168b2f34d13f8bcc69570ed5dac9ad16d4842
Charlotte Van Herwijnen <cml.v.herwijnen@gmail.com>
2020-07-15 15:47:04 +0100

    While Loops Viedo notes from Coursera

---
+++ While Loops.R
@@ -0,0 +1,18 @@
+# While Loops and Repeat Loops in R
+
+# While loosp in R are essentially the same thing in other programms, it will repeat the specified command as long as
+# the condtion specified in the While Loop remains true
+
+#The general format of a While Loop is 
+
+# while(condition specified){
+#   While loop commands
+#}
+
+# A very simple example is as such
+
+x = 0
+while(x<101){
+  print(x)
+  x = x+1
+}
